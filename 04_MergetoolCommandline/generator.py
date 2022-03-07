e5368e826896f8c64746aa20a5feafa7a89967bd
Nasty <bogatenkova.anastasiya@mail.ru>
2022-03-07 22:54:40 +0300

    HW-4 add completion to generators

--- 04_MergetoolCommandline/generator.py
+++ 04_MergetoolCommandline/generator.py
@@ -18,14 +18,17 @@ class NameGenerator(cmd.Cmd):
             "female": pynames.GENDER.FEMALE
         }
         self.classes = defaultdict(dict)
+        self.languages = []
 
         for module_name, module_data in inspect.getmembers(pynames.generators, inspect.ismodule):
             for class_name, class_data in inspect.getmembers(module_data, inspect.isclass):
                 if class_name.endswith("Generator"):
                     try:
-                        self.classes[module_name.lower()][class_name.lower()] = class_data()
+                        self.classes[module_name.lower()][self.__strip_name(class_name.lower())] = class_data()
+                        self.languages.extend(list(class_data().languages))
                     except (NotImplementedError, TypeError):
                         pass
+        self.languages = list(set(self.languages))
 
     def do_generate(self, arg):
         """
@@ -44,6 +47,18 @@ class NameGenerator(cmd.Cmd):
         except KeyError:
             print(generator.get_name_simple(gender, "native"))
 
+    def complete_generate(self, prefix, line, beg_idx, end_idx):
+        prefix = prefix.lower().strip()
+        words = line.lower().split()
+        if len(words) == 4 or (len(words) == 3 and line.endswith(" ")):
+            return [completion for completion in list(self.genders) if completion.startswith(prefix)]
+        if len(words) == 3 or (len(words) == 2 and line.endswith(" ")):
+            completion_list = list(self.classes[words[1]]) + list(self.genders) if words[1] in self.classes else list(self.genders)
+            return [completion for completion in completion_list if completion.startswith(prefix)]
+        if len(words) == 2 or (len(words) == 1 and line.endswith(" ")):
+            return [completion for completion in self.classes if completion.startswith(prefix)]
+        return []
+
     def do_info(self, arg):
         """
         Get the number of names or the list of languages
@@ -59,7 +74,8 @@ class NameGenerator(cmd.Cmd):
             generator = self.__get_generator(args)
             if generator is not None:
                 print(" ".join(generator.languages))
-            print("Invalid arguments")
+            else:
+                print("Invalid arguments")
         else:
             generator = self.__get_generator(args)
             if generator is not None:
@@ -70,6 +86,19 @@ class NameGenerator(cmd.Cmd):
             else:
                 print("Invalid arguments")
 
+    def complete_info(self, prefix, line, beg_idx, end_idx):
+        prefix = prefix.lower().strip()
+        words = line.lower().split()
+        if len(words) == 4 or (len(words) == 3 and line.endswith(" ")):
+            return [completion for completion in list(self.genders) + ['language'] if completion.startswith(prefix)]
+        if len(words) == 3 or (len(words) == 2 and line.endswith(" ")):
+            completion_list = list(self.classes[words[1]]) + list(self.genders) + ['language'] \
+                if words[1] in self.classes else list(self.genders) + ['language']
+            return [completion for completion in completion_list if completion.startswith(prefix)]
+        if len(words) == 2 or (len(words) == 1 and line.endswith(" ")):
+            return [completion for completion in self.classes if completion.startswith(prefix)]
+        return []
+
     def do_language(self, arg):
         """
         Set the language: language RU
@@ -77,9 +106,21 @@ class NameGenerator(cmd.Cmd):
         If the language not supported, native is used
         """
         args = arg.lower().strip().split(" ")
-        if len(args) != 1:
-            print("Invalid arguments")
-        self.language = args[0]
+        if len(args) != 1 or args[0] not in self.languages:
+            print(f"Invalid arguments, {' '.join(self.languages)} are available")
+        else:
+            self.language = args[0]
+
+    def complete_language(self, prefix, line, beg_idx, end_idx):
+        return [completion for completion in self.languages if completion.startswith(prefix.lower().strip())]
+
+    def __strip_name(self, class_name):
+        if class_name.endswith('namesgenerator'):
+            return class_name[:-len('namesgenerator')]
+        if class_name.endswith('fullnamegenerator'):
+            return class_name[:-len('fullnamegenerator')]
+        if class_name.endswith('generator'):
+            return class_name[:-len('generator')]
 
     def __get_generator(self, args):
         cls = None
@@ -103,9 +144,8 @@ class NameGenerator(cmd.Cmd):
     def __get_subclass(self, class_name, subclass_name):
         if class_name not in self.classes:
             return
-        for cls_name in self.classes[class_name]:
-            if cls_name.startswith(subclass_name):
-                return self.classes[class_name][cls_name]
+        if subclass_name in self.classes[class_name]:
+            return self.classes[class_name][subclass_name]
 
 
 if __name__ == "__main__":
